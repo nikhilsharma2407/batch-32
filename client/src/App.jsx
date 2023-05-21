9ca5bb2158140b91bde2580170f732c661300a78
Nikhil Sharma <nikhil.sharma2407@gmail.com>
2023-05-21 22:20:52 +0530

    Day 4 - Walkthrough of the project, Intro to react, react life cycle

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import ComponentA from './ComponentA';
+import { useState } from 'react';
+
+function App() {
+  const name = "Nikhil";
+  const [showComponent, setShowComponent] = useState(true);
+  return (
+    <div className="App">
+      <h1>Hello {name}</h1>
+      <button onClick={()=>setShowComponent(!showComponent)}>{showComponent?'Hide Component':'Show Compoent'}</button>
+      {showComponent?<ComponentA name={name} id={101} />:null}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/ComponentA/index.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+
+export default class ComponentA extends Component {
+
+    constructor() {
+        super();
+        console.log("initialisation constructor");
+        this.state = { greeting: 'Learning React' }
+    }
+
+    render() {
+        console.log('running render');
+        console.log(this.props);
+        const name = this.props.name;
+
+        // error
+        // this.props.name = "something else"
+
+        const greet = () => {
+            alert('Hello ' + name)
+        }
+
+        // function updateTitle(){
+
+        //     this.setState({title:'Learning React is Fun!!!'})
+        // }
+        const updateGreeting = ()=>{
+            this.setState({greeting:'Learning React is Fun!!!'})
+        }
+
+
+        const titleCode = <h1>{this.state.greeting}</h1>
+
+        return (
+            <>
+                {titleCode}
+                {1 + 1}
+                <h2>React is easy</h2>
+                <button onClick={() => alert('Hello ' + name)}>Click me</button>
+                <button onClick={updateGreeting}>Update Greeting</button>
+            </>
+        )
+    }
+
+    componentWillMount(){
+        console.log('componentWillMount')
+    }
+    componentDidMount(){
+        console.log('componentDidMount');
+        console.log('updating title');
+        document.title = 'ComponentA'
+    }
+    shouldComponentUpdate(){
+        console.log('shouldComponentUpdate');
+        return true;
+    }
+    componentWillUpdate(){
+        console.log('componentWillUpdate')
+    }
+    componentDidUpdate(){
+        console.log('componentDidUpdate')
+    }
+
+    componentWillUnmount(){
+        console.log('Unmounting');
+    }
+}
